1f3530974155dabec181463e695970070eba21be
kaliglass <146648840+kaliglass@users.noreply.github.com>
2024-03-24 23:45:50 +0530

    Added the hover effectfor all the grid.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector('.container');
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        
         container.appendChild(square);
-    }
-    
+        square.addEventListener("mouseover",()=>{
+            // square.setAttribute("style","background-color:blue;");
+            square.style.backgroundColor = "black";
+        });
+    }   
 }
+
