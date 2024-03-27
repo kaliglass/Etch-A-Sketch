ae14eac5a833bc245bd04582c808726fd9d4656c
kaliglass <146648840+kaliglass@users.noreply.github.com>
2024-03-27 20:54:31 +0530

    Added a nice UI for the button and gave a background color

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ for (let i = 0; i < gridNo; i++) {
         square.style.width = `${800/gridNo}px`;
         square.style.height = `${800/gridNo}px`;
         square.style.border = "1px solid black";
+        square.style.backgroundColor = "white";
         container.appendChild(square);
         square.addEventListener("mouseover",()=>{
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
